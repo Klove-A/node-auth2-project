c9d3413307ebdf4aed2fba6dc6750bd8072ca9af
Klove Adams <kloveadams@gmail.com>
2021-10-12 15:15:44 -0400

    find works as intended

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -18,7 +18,7 @@ function find() {
       }
     ]
    */
-  return db("users as u").join("roles as r").select("u.user_id", "u.username", "r.role_name")
+  return db("users as u").join("roles as r", "u.role_id", "r.role_id").select("u.user_id", "u.username", "r.role_name")
 }
 
 function findBy(filter) {
